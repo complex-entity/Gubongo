603884f08032ab5d9cb7f2c5b33ce1974a7dfa61
Molnár Márk <molnar.mark@netgo.hu>
2019-02-09 19:13:20 +0100

    Fizika és ütközések

--- gubongo_v1.js
+++ gubongo_v1.js
@@ -1,14 +1,18 @@
 const config = {
-    type: Phaser.AUTO,
+    type: Phaser.WEBGL,
     width: 1820,
-    height: 1000,
+    height: 1020,
     parent: "game-container",
     pixelArt: true,
     scene: {
       preload: preload,
       create: create,
       update: update
-    }
+    },
+    physics: {
+        default: 'impact',
+        impact: { gravity: 200 }
+    },
   };
   
   const game = new Phaser.Game(config);
@@ -17,6 +21,7 @@ const config = {
   function preload() {
     this.load.image("tiles", "assets/sprites/spritesheet.png");
     this.load.tilemapTiledJSON("map", "assets/tilemap/gubongo_map.json");
+    this.load.spritesheet('player', 'assets/sprites/kukacok.png',{ frameWidth: 32, frameHeight: 32 });
   }
   
   function create() {
@@ -32,30 +37,51 @@ const config = {
     const sky = map.createStaticLayer("sky", tileset, 0, 0);
     const bg = map.createStaticLayer("bg", tileset, 0, 0);
     const notcollidingitems = map.createStaticLayer("notcollidingitems", tileset, 0, 0);
-    const platforms = map.createStaticLayer("platforms", tileset, 0, 0);
     const ground = map.createStaticLayer("ground", tileset, 0, 0);
 
-    // Phaser supports multiple cameras, but you can access the default camera like this:
-    const camera = this.cameras.main;
-  
-    // Set up the arrows to control the camera
-    const cursors = this.input.keyboard.createCursorKeys();
-    controls = new Phaser.Cameras.Controls.FixedKeyControl({
-      camera: camera,
-      left: cursors.left,
-      right: cursors.right,
-      up: cursors.up,
-      down: cursors.down,
-      speed: 0.5
-    });
-  
-    // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
-    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    camera.setBackgroundColor('rgba(208, 244, 247, 1)');
+    ground.setCollisionByProperty({ collides: true });
+
+    this.impact.world.setCollisionMapFromTilemapLayer(ground, { slopeProperty: 'slope' });
+
+    player = this.impact.add.sprite(100, 32,'player');
+    player.setMaxVelocity(300, 400).setFriction(800, 0);
+    player.body.accelGround = 1200;
+    player.body.accelAir = 600;
+    player.body.jumpSpeed = 300;
+
+    var style = { font: "12px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: player.width, align: "center", backgroundColor: "transparent" };
+
+    text = this.add.text(0, 0, "kukacka", style);
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    //this.cameras.main.startFollow(player);
+
+    cursors = this.input.keyboard.createCursorKeys();
+    //camera.setBackgroundColor('rgba(208, 244, 247, 1)');
   
   }
   
   function update(time, delta) {
-    // Apply the controls to the camera each update tick of the game
-    controls.update(delta);
+    var accel = player.body.standing ? player.body.accelGround : player.body.accelAir;
+
+    if (cursors.left.isDown)
+    {
+        player.setAccelerationX(-accel);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setAccelerationX(accel);
+    }
+    else
+    {
+        player.setAccelerationX(0);
+    }
+
+    if (cursors.up.isDown && player.body.standing)
+    {
+        player.setVelocityY(-player.body.jumpSpeed);
+    }
+
+    text.x = Math.floor(player.x + player.width / 2);
+    text.y = Math.floor(player.y + player.height / 2);
   }
